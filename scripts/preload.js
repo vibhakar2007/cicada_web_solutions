062aa6f7d7c2acfb9a44bf267ee220f62917cb29
Vibhakar S <vibhakarsenth@gmail.com>
2026-02-08 13:07:58 +0530

    Preloading Added

---
+++ scripts/preload.js
@@ -0,0 +1,33 @@
+const assets = document.images;
+let loaded = 0;
+
+gsap.set(".progress", { scaleX: 0 });
+gsap.to(".loader-logo", { opacity: 1, scale: 1, duration: 0.8 });
+
+Array.from(assets).forEach(img => {
+  if (img.complete) update();
+  else img.addEventListener("load", update);
+});
+
+function update() {
+  loaded++;
+  const progress = loaded / assets.length;
+
+  gsap.to(".progress", {
+    scaleX: progress,
+    duration: 0.3,
+    ease: "power2.out"
+  });
+
+  if (progress === 1) finish();
+}
+
+function finish() {
+  gsap.to("#loader", {
+    yPercent: -100,
+    duration: 0.9,
+    ease: "power4.inOut",
+    delay: 0.2,
+    onComplete: () => loader.remove()
+  });
+}
