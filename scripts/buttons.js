984b13594da5a4a0d3b649ae8fda4da51594277a
Vibhakar S <vibhakarsenth@gmail.com>
2026-02-28 14:53:37 +0530

    Removed Teams Section and Added Button Functionality

---
+++ scripts/buttons.js
@@ -0,0 +1,15 @@
+contactButton = document.getElementById("contactButton");
+pcContactButton = document.getElementById("pcContactButton");
+initializeProject = document.getElementById("initializeProject");
+
+contactButton.addEventListener("click", () => {
+    window.location.href = "https://wa.me/+918778688185";
+})
+
+pcContactButton.addEventListener("click", () => {
+    window.location.href = "https://wa.me/+918778688185";
+})
+
+initializeProject.addEventListener("click", () => {
+    window.location.href = "https://wa.me/+918778688185";
+})
